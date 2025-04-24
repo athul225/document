4f74e43da9c2e522f7c99d48be1388a69b1ea9d7
athul225 <athulviju599gmail.com>
2025-04-24 15:59:56 +0530

    newdocument

---
+++ javascript/dom.js
@@ -0,0 +1,9 @@
+const paragraphs=document.getElementsByTagName('p')
+for (i of paragraphs){
+    console.log(i.textContent)
+}
+
+const para=document.getElementsByClassName('aa')
+for (i of para){
+    console.log(i.textContent)
+}
